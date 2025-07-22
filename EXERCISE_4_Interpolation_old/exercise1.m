5f713f008f421dab89dd2c15c233d3765a2f9d4f
Tasnimul Hasan Prottoy <67048618+tasnimul11@users.noreply.github.com>
2025-07-22 10:55:58 +0600

    Create exercise1.m

---
+++ EXERCISE_4_Interpolation_old/exercise1.m
@@ -0,0 +1,11 @@
+A = poly([3 2 -4]);         % for 3x^2 + 2x - 4
+B = poly([2 0 0 -2]);       % for 2x^3 -2
+
+C = conv(A,B) % co-efficients of C(x) = A(x)*B(x)
+syms x;
+C_eq = poly2sym(C, x)
+
+% finding roots
+R_A = roots(A)
+R_B = roots(B)
+R_C = roots(C)
